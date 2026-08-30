bd6129f4cc911def3543c98f60cf04ae535cd880
spch321-sudo <spch321@gmail.com>
2026-08-30 17:15:24 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'g321theo2-v1.0.4';
+const CACHE_VERSION = 'g321theo2-v1.0.6';
 const CORE = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
